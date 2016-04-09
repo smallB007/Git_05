d951d9e96215488fa52c2731ce42608546948737
artie <atch.cpp@gmail.com>
2016-04-09 18:11:17 +0200

    Added files before merge to master

--- Git_05/HTTPS_GIT_Client.h
+++ Git_05/HTTPS_GIT_Client.h
@@ -2,7 +2,9 @@
 #include <vector>
 #include <memory>
 #include <iostream>
+#include <map>
 #include <set>
+
 
 #include "Poco/StreamCopier.h"
 #include "Poco/URI.h"
@@ -18,17 +20,43 @@
 #include "Poco/JSON/Array.h"
 #include "Poco/JSON/Parser.h"
 
+#include "Git_User.hpp"
 #include "Git_Repository.hpp"
+
+template<class T>
+struct Less;
+template<>
+struct Less<Git_User>
+{
+	bool operator()(const Git_User& left, const Git_User& right)
+	{
+		return left.get_attribute("name") < right.get_attribute("name");
+	}
+};
+template<>
+struct Less<Git_Repository>
+{
+	bool operator()(const Git_Repository& left, const Git_Repository& right)
+	{
+		return left.get_attribute("name") < right.get_attribute("name");
+	}
+};
+
+typedef Poco::JSON::Array JSON_Array;
 
 class HTTPS_GIT_Client
 {
+	typedef std::map<Git_User, std::set<Git_Repository,Less<Git_Repository>>, Less<Git_User>> user_repos_t;
+
 private:
-	std::set<Git_Repository> repositories_;
-	void get_repositories_();
+	user_repos_t user_repos_;
+	void get_user_repositories_(const Git_User& user);
+	void fill_json_data_(const Poco::JSON::Object::Ptr& arr, Git_Object & git_object);
+
 public:
 	HTTPS_GIT_Client();
 	~HTTPS_GIT_Client();
 	void connect();
-	decltype(repositories_) repositories()const;
+	std::set<Git_Repository,Less<Git_Repository>> user_repositories(const Git_User& user)const;
 };
 
