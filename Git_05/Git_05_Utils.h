408e29aa6b729c4e4231a37050797e31a83c0372
artie <atch.cpp@gmail.com>
2016-04-12 19:40:55 +0200

    added UML diagram

--- Git_05/Git_05_Utils.h
+++ Git_05/Git_05_Utils.h
@@ -3,6 +3,9 @@
 #include <atlconv.h>
 #include <string>
 
+//https://msdn.microsoft.com/en-us/library/87zae4a3.aspx
+//http://stackoverflow.com/questions/2573834/c-convert-string-or-char-to-wstring-or-wchar-t
+
 inline std::wstring string_to_wstring(const std::string& str)
 {
 	CA2W ca2w(str.c_str());
